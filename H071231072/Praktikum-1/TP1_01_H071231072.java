22a240efa1395001002cd95b68ed977cbfd59268
ahmadrafly2023 <ahmd.rfly19@gmail.com>
2024-03-21 22:05:27 +0800

    kumpul tugas 1

---
+++ H071231072/Praktikum-1/TP1_01_H071231072.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class TP1_01_H071231072 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jumlah bilangan: ");
+        int x = scanner.nextInt();
+        scanner.nextLine(); 
+
+        int decimalCount = 0;
+        int integerCount = 0;
+
+        for (int i = 0; i < x; i++) {
+            System.out.print("Masukkan bilangan ke-" + (i + 1) + ": ");
+            String numInput = scanner.nextLine();
+
+            try {
+                double num = Double.parseDouble(numInput);
+
+                if (num % 1 == 0) {
+                    integerCount++;
+                } else {
+                    decimalCount++;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Input tidak valid: " + numInput);
+            }
+        }
+
+        System.out.println(integerCount + " bilangan bulat");
+        System.out.println(decimalCount + " bilangan desimal");
+    }
+}
---
+++ H071231072/Praktikum-1/TP1_05_H071231072.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class TP1_05_H071231072 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] nums = {{1,2,3}, {4,5,6}, {7,8,9}};
+        System.out.println("Masukkan bilangan: ");
+        try {
+            int target = scanner.nextInt();
+            boolean found = false;
+
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = 0; j < nums[i].length; j++) {
+                    if (nums[i][j] == target) {
+                        System.out.println("Found " + target + " at [" + i + "][" + j + "]");
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) break;
+            }
+
+            if (!found) {
+                System.out.println("Bilangan tidak ditemukan");
+            }
+        } catch (Exception e) {
+            System.out.println("Input harus berupa angka.");
+        }
+
+        scanner.close();
+    }
+}
